5adb823d69e0b668c24f5fca408f01d6a5bd2556
abdulazizladan <abdulazizladan@gmail.com>
2021-03-08 12:59:30 +0100

    feat: Add user entity

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { Product } from './entity/product.entity';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [ProductsService],
+  imports: [TypeOrmModule.forFeature([Product])],
+  exports: [TypeOrmModule],
 })
 export class ProductsModule {}
